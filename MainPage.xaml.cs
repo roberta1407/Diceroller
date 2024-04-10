a6ebac2a17159757878612e2202b197d04b3bb4f
roberta1407 <162491668+roberta1407@users.noreply.github.com>
2024-04-10 15:43:30 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RollButton_Clicked(object sender, EventArgs e)
+        {
+            int picker = DicePicker.SelectedIndex == 0 ? 6 : 10;
+            int result = new Random().Next(1, picker + 1);
+            ResultLabel.Text = $"Rolando: {result}";
+        }
+    }
+}
+
